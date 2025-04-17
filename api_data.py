1531921480112d78ff7180ccf3c60bcd7db7db27
Solomon Antony <santony@murraystate.edu>
2025-04-17 10:54:43 -0500

    updated with points

--- api_data.py
+++ api_data.py
@@ -1,13 +1,13 @@
 """
 Task 1: Select an API server from the list 
 at https://github.com/public-api-lists/public-api-lists.  Submit your choice 
-of the public API a text entry by 04/23/2025.
+of the public API a text entry by 04/23/2025. (5 points)
 Task 2: 
 Create a script that (i) displays a brief description of the 
 API data, (ii) lists what variable the user can query on, 
 (iii) prompts the user for an input to use as a query, 
 (iv) displays results of the query in an easy format, and  
-(v) Allow the user try multiple times.  
+(v) Allow the user try multiple times.  (25 points)
 
 Make sure your script is robust, and has proper exception handling mechanisms.
 """

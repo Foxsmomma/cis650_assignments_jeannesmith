1c95c2b356370f3a92e4a1fa108af4ad66d21302
niceguysolo <niceguysolo@gmail.com>
2025-06-21 23:32:03 -0500

    updated for summer

--- api_data.py
+++ api_data.py
@@ -1,8 +1,7 @@
 """
 Task 1: Select an API server from the list 
-at https://github.com/public-api-lists/public-api-lists.  Submit your choice 
-of the public API a text entry by 04/23/2025. (5 points)
-Task 2: 
+at https://github.com/public-api-lists/public-api-lists.  
+
 Create a script that (i) displays a brief description of the 
 API data, (ii) lists what variable the user can query on, 
 (iii) prompts the user for an input to use as a query, 

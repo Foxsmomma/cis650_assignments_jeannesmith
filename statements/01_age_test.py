22f3972d422f490f398eb545fa81971007b8c4f1
Foxsmomma <60080331+Foxsmomma@users.noreply.github.com>
2025-06-28 16:55:10 +0000

    new file:   cis650_notes 	modified:   statements/01_age_test.py 	modified:   statements/05_grocery_bill.py 	modified:   statements/02_magic.py     modified:   statements/03_numbers.py     modified:   statements/04_tax_bracket.py

--- statements/01_age_test.py
+++ statements/01_age_test.py
@@ -3,7 +3,7 @@ name = input('What is your name?')
 age  = int(input('What is your age?'))
 if name == 'Alice':
     print('Hi, Alice.')
-elif age < 12:
+elif age <0:
     print('You are not Alice, kiddo.')
 elif age > 2000:
     print('Unlike you, Alice is not an undead, immortal vampire.')
--- statements/05_grocery_bill.py
+++ statements/05_grocery_bill.py
@@ -5,6 +5,17 @@
 # Make the output easy to read on the terminal.
 # Do not use any data structures or any package for display
 
+def numItems = input ("Please enter the number of items:")
+itemPrint = ""
+totalPrice = 0
+
+for num in range(1, NumItems + 1):
+    itemName = input("Please enter the name of item:")
+    itemPrice = input("Please enter the item price")
+    itemPrint = itemPrint + "Item: " + itemName + " Price: " + itemPrice + "\n"
+    totalPrice += itemPrice
+
+print itemPrint + "\nNumber of items: " + numItems + "\nTotal Price: " + totalPrice
 
 
 #git commit -m 'updated 05_grocery_bill' and then

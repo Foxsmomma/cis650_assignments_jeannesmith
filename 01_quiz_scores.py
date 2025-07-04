b1d9114f598ca8dcda0eb2a80620b30af25bae47
Foxsmomma <60080331+Foxsmomma@users.noreply.github.com>
2025-07-04 19:53:49 +0000

    quiz scores complete

--- 01_quiz_scores.py
+++ 01_quiz_scores.py
@@ -2,8 +2,6 @@ student_quiz_scores = [['John', 12, 20],
             ['Rita', 23, 12],
             ['Ganesh', 20, 15]]
 for row in student_quiz_scores:
-  for cell in row:
-    print(cell, end='|')
-  print()
+    print(row [0],"Sum=",row[1]+row[2])
 #Modify the code so that it prints the sum of two scores for each student
 

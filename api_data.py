65c741ff838ac616c5ae2bd2c0ecd7a2e7594908
Foxsmomma <60080331+Foxsmomma@users.noreply.github.com>
2025-08-14 21:26:32 +0000

    updated

--- api_data.py
+++ api_data.py
@@ -1,13 +1,23 @@
-"""
-Task 1: Select an API server from the list 
-at https://github.com/public-api-lists/public-api-lists.  
 
-Create a script that (i) displays a brief description of the 
-API data, (ii) lists what variable the user can query on, 
-(iii) prompts the user for an input to use as a query, 
-(iv) displays results of the query in an easy format, and  
-(v) Allow the user try multiple times.  (25 points)
 
-Make sure your script is robust, and has proper exception handling mechanisms.
-# Git commit with 'updated ap_data' and push 
-"""
+import requests
+from bs4 import BeautifulSoup
+url = 'https://cataas.com/'
+page = requests.get(url)
+print(page)
+soup = BeautifulSoup(page.content, 'html.parser')
+links = soup.find_all('a')
+for link in links:
+  print(link)
+
+
+soup.find_all('span')[0]
+# display the title of page
+#print(soup.title.text.strip())
+for link in soup.find_all('a'):
+  #print(link['href'])
+  print(link.string, link['href'])
+  #print(link.get('href'))
+  
+
+
